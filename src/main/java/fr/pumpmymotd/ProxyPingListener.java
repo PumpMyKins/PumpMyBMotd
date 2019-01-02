ce4829d7c8b4caa096ea6ee05c75168b80a89246
Clem-Fern <clement.coussieu@gmail.com>
2019-01-03 00:42:05 +0100

    Update import in PingManager class

--- src/main/java/fr/pumpmymotd/ProxyPingListener.java
+++ src/main/java/fr/pumpmymotd/ProxyPingListener.java
@@ -1,6 +1,6 @@
 package fr.pumpmymotd;
 
-import fr.pumpmymotd.motd.MotdManager;
+import fr.pumpmymotd.motd.PingManager;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
