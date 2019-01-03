b0ca6529d50281dda261d2e713c7309f62dcb95b
Clem-Fern <clement.coussieu@gmail.com>
2019-01-03 01:20:36 +0100

    Add default ping field in PingManager class

--- src/main/java/fr/pumpmymotd/motd/PingManager.java
+++ src/main/java/fr/pumpmymotd/motd/PingManager.java
@@ -8,12 +8,15 @@ import net.md_5.bungee.config.Configuration;
 public class PingManager {
 	
 	private HashMap<String, Ping> pings;
+	private Ping defaultPing;
 	private ConfigUtils config;
 
 	public PingManager(ConfigUtils config) {
 		
 		this.pings = new HashMap<String, Ping>();
 		this.config = config;
+		
+		this.defaultPing = null;
 		
 	}
 
