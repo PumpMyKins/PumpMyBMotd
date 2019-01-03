9fd16deefe5635dc708e57253907f68f42e79888
Clem-Fern <clement.coussieu@gmail.com>
2019-01-03 01:19:52 +0100

    Add builder patern intra class in Ping class

--- src/main/java/fr/pumpmymotd/motd/Ping.java
+++ src/main/java/fr/pumpmymotd/motd/Ping.java
@@ -2,4 +2,18 @@ package fr.pumpmymotd.motd;
 
 public class Ping {
 
+	private Ping() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static class PingBuilder {
+		
+		public Ping build() {
+			
+			return new Ping();
+			
+		}
+		
+	}
+	
 }
