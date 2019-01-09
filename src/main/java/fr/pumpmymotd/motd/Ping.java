06a5ff01f23b0635689ae4efb2dc1218f4fba125
Clem-Fern <clement.coussieu@gmail.com>
2019-01-09 21:02:44 +0100

    Update builder & add getters/fields in Ping class

--- src/main/java/fr/pumpmymotd/motd/Ping.java
+++ src/main/java/fr/pumpmymotd/motd/Ping.java
@@ -4,15 +4,56 @@ import java.io.File;
 
 public class Ping {
 
-	private Ping() {
-		// TODO Auto-generated constructor stub
-	}
+	private String line1;
+	private String line2;
 	
+	private File favicon;
+	
+	public Ping(String line1, String line2, File favicon) {
+		
+		this.line1 = line1;
+		this.line2 = line2;
+		this.favicon = favicon;
+		
+	}
+
+	public String getLine1() {
+		return line1;
+	}
+
+	public String getLine2() {
+		return line2;
+	}
+
+	public File getFavicon() {
+		return favicon;
+	}
+
 	public static class PingBuilder {
+		
+		private String line1;
+		private String line2;
+		
+		private File favicon;
+		
+		public PingBuilder setLine1(String line1) {
+			this.line1 = line1;
+			return this;
+		}
+		
+		public PingBuilder setLine2(String line2) {
+			this.line2 = line2;
+			return this;
+		}
+
+		public PingBuilder setFavicon(File favicon) {
+			this.favicon = favicon;
+			return this;
+		}
 		
 		public Ping build() {
 			
-			return new Ping();
+			return new Ping(this.line1,this.line2,this.favicon);
 			
 		}
 		
