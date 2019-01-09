a4403e4026204a9df8e804a8fbb24f73424d27ab
Clem-Fern <clement.coussieu@gmail.com>
2019-01-09 23:02:02 +0100

    Update server ping back

--- src/main/java/fr/pumpmymotd/ProxyPingListener.java
+++ src/main/java/fr/pumpmymotd/ProxyPingListener.java
@@ -4,6 +4,7 @@ import fr.pumpmymotd.motd.Ping;
 import fr.pumpmymotd.motd.PingManager;
 import net.md_5.bungee.api.ServerPing;
 import net.md_5.bungee.api.ServerPing.Protocol;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -28,7 +29,8 @@ public class ProxyPingListener implements Listener {
 		
 		ServerPing serverPing = event.getResponse();
 		
-		serverPing.setVersion(new Protocol("TEST", serverPing.getVersion().getProtocol()));
+		serverPing.setDescription("Test");
+		serverPing.setVersion(new Protocol("Reboot en cours", -1));
 		
 		event.setResponse(serverPing);				
 		
