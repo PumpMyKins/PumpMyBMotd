9f1639a66e5e61b2a546c1a672447ae42f979d46
Clem-Fern <clement.coussieu@gmail.com>
2019-01-10 19:38:26 +0100

    Update motd setting & favicon getting in ProxyPingEvent

--- src/main/java/fr/pumpmymotd/ProxyPingListener.java
+++ src/main/java/fr/pumpmymotd/ProxyPingListener.java
@@ -1,10 +1,16 @@
 package fr.pumpmymotd;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import fr.pumpmymotd.motd.Ping;
 import fr.pumpmymotd.motd.PingManager;
+import net.md_5.bungee.api.Favicon;
 import net.md_5.bungee.api.ServerPing;
-import net.md_5.bungee.api.ServerPing.Protocol;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.event.ProxyPingEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -22,14 +28,21 @@ public class ProxyPingListener implements Listener {
 	}
 	
 	@EventHandler
-	public void onProxyPing(ProxyPingEvent event) {
+	public void onProxyPing(ProxyPingEvent event) throws IOException {
 		
-		String host = event.getConnection().getVirtualHost().getHostString();
+		PendingConnection con = event.getConnection();
+		if(con == null)
+			return;
+		
+		String host = con.getVirtualHost().getHostString();
 		Ping ping = this.manager.getPing(host);
 		
 		ServerPing serverPing = event.getResponse();
 		
-		serverPing.setVersion(new Protocol("Spigot 1.2.3", 1));
+		serverPing.setDescriptionComponent(new TextComponent(ping.getLine1() + "\n" + ping.getLine2()));		
+		
+		BufferedImage img = ImageIO.read(ping.getFavicon());		
+		serverPing.setFavicon(Favicon.create(img));
 		
 		event.setResponse(serverPing);				
 		
